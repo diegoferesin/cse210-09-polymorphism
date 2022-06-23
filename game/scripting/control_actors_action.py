d95ac24d351399e3bdb553382ca9795f7aad106e
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-06-23 17:46:45 -0400

    sankes grow when move to different direction

--- game/scripting/control_actors_action.py
+++ game/scripting/control_actors_action.py
@@ -30,41 +30,49 @@ class ControlActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        
+        snake = cast.get_first_actor("snakes")
         # left
         if self._keyboard_service.is_key_down('a'):
             self._player1_direction = Point(-constants.CELL_SIZE, 0)
+            snake.grow_tail(1)
         
         # right
         if self._keyboard_service.is_key_down('d'):
             self._player1_direction = Point(constants.CELL_SIZE, 0)
-        
+            snake.grow_tail(1)
+
         # up
         if self._keyboard_service.is_key_down('w'):
             self._player1_direction = Point(0, -constants.CELL_SIZE)
-        
+            snake.grow_tail(1)
+
         # down
         if self._keyboard_service.is_key_down('s'):
             self._player1_direction = Point(0, constants.CELL_SIZE)
-        
+            snake.grow_tail(1)
+
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._player1_direction)
 
         # left
         if self._keyboard_service.is_key_down('j'):
             self._player2_direction = Point(-constants.CELL_SIZE, 0)
+            snake.grow_tail(1)
         
         # right
         if self._keyboard_service.is_key_down('l'):
             self._player2_direction = Point(constants.CELL_SIZE, 0)
+            snake.grow_tail(1)
         
         # up
         if self._keyboard_service.is_key_down('i'):
             self._player2_direction = Point(0, -constants.CELL_SIZE)
+            snake.grow_tail(1)
         
         # down
         if self._keyboard_service.is_key_down('k'):
             self._player2_direction = Point(0, constants.CELL_SIZE)
+            snake.grow_tail(1)
         
         snake2 = cast.get_actors("snakes")[1]
         snake2.turn_head(self._player2_direction)
