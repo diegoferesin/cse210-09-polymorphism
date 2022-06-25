8ddadf940daab9666c5ad9c7ca145fcad96acbeb
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-06-25 19:23:25 -0400

    colllisions try update

--- game/scripting/handle_collisions_action.py
+++ game/scripting/handle_collisions_action.py
@@ -69,17 +69,23 @@ class HandleCollisionsAction(Action):
         """
         snake = cast.get_first_actor("snakes")
         snake2 = cast.get_second_actor("snakes")
-        head = snake.get_segments()[0]
-        head2 = snake2.get_segments()[0]
+        head = snake.get_head()
+        head2 = snake2.get_head()
         segments = snake.get_segments()[1:]
         segments2 = snake2.get_segments()[1:]
 
         for segment in segments:
             if head.get_position().equals(segment.get_position()):
                 self._is_game_over = True
+            
+            if head2.get_position().equals(segment.get_position()):
+                self._is_game_over = True
                 
         for segment in segments2:
             if head2.get_position().equals(segment.get_position()):
+                self._is_game_over = True
+            
+            if head.get_position().equals(segment.get_position()):
                 self._is_game_over = True
 
     def _handle_game_over(self, cast):
